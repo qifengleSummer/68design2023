aa0ca3a534f18181737aac244ac5ac1d8e024ee0
qifengleSummer <b654820442@163.com>
2023-03-20 17:35:56 +0800

    修改作品集合通过接口返回

--- expressIndex.js
+++ expressIndex.js
@@ -10,16 +10,144 @@ app.all('*', function (req, res, next) {
   next()
 })
 
+const resConfig = (data) => {
+  return {
+    success: 'success',
+    code: 200,
+    msg: '',
+    data: data,
+  }
+}
+
 app.get('/workItemDetailList', (req, res) => {
-  setTimeout(() => {
-    res.send('hello world')
-  }, 1000)
+  res.json(
+    resConfig([
+      {
+        title: '随风集',
+        author: '金昌友',
+        type: '网页，企业官网',
+        thumb: '57',
+        eye: '1990',
+        publishTime: '22小时前',
+      },
+      {
+        title: '草木茶舍 | 泡茶古朴又惊艳',
+        author: '黄冠之',
+        type: '空间，空间餐饮',
+        thumb: '69',
+        eye: '3018',
+        publishTime: '1天前',
+      },
+      {
+        title: '字体设计 | Font design',
+        author: '戴秀',
+        type: '平面，字体',
+        thumb: '157',
+        eye: '6990',
+        publishTime: '2天前',
+      },
+      {
+        title: '随风集',
+        author: '金昌友',
+        type: '网页，企业官网',
+        thumb: '57',
+        eye: '1990',
+        publishTime: '22小时前',
+      },
+      {
+        title: '随风集',
+        author: '金昌友',
+        type: '网页，企业官网',
+        thumb: '57',
+        eye: '1990',
+        publishTime: '22小时前',
+      },
+      {
+        title: '随风集',
+        author: '金昌友',
+        type: '网页，企业官网',
+        thumb: '57',
+        eye: '1990',
+        publishTime: '22小时前',
+      },
+      {
+        title: '随风集',
+        author: '金昌友',
+        type: '网页，企业官网',
+        thumb: '57',
+        eye: '1990',
+        publishTime: '22小时前',
+      },
+      {
+        title: '随风集',
+        author: '金昌友',
+        type: '网页，企业官网',
+        thumb: '57',
+        eye: '1990',
+        publishTime: '22小时前',
+      },
+      {
+        title: '随风集',
+        author: '金昌友',
+        type: '网页，企业官网',
+        thumb: '57',
+        eye: '1990',
+        publishTime: '22小时前',
+      },
+      {
+        title: '随风集',
+        author: '金昌友',
+        type: '网页，企业官网',
+        thumb: '57',
+        eye: '1990',
+        publishTime: '22小时前',
+      },
+      {
+        title: '随风集',
+        author: '金昌友',
+        type: '网页，企业官网',
+        thumb: '57',
+        eye: '1990',
+        publishTime: '22小时前',
+      },
+      {
+        title: '随风集',
+        author: '金昌友',
+        type: '网页，企业官网',
+        thumb: '57',
+        eye: '1990',
+        publishTime: '22小时前',
+      },
+      {
+        title: '随风集',
+        author: '金昌友',
+        type: '网页，企业官网',
+        thumb: '57',
+        eye: '1990',
+        publishTime: '22小时前',
+      },
+      {
+        title: '随风集',
+        author: '金昌友',
+        type: '网页，企业官网',
+        thumb: '57',
+        eye: '1990',
+        publishTime: '22小时前',
+      },
+      {
+        title: '随风集',
+        author: '金昌友',
+        type: '网页，企业官网',
+        thumb: '57',
+        eye: '1990',
+        publishTime: '22小时前',
+      },
+    ])
+  )
 })
 
 app.post('/testPost', (req, res) => {
-  setTimeout(() => {
-    res.send('hello testPost')
-  }, 1000)
+  res.send(resConfig('hello testPost'))
 })
 
 app.listen(port, () => {
--- src/pages/components/workItemDetail/WorkItemDetail.js
+++ src/pages/components/workItemDetail/WorkItemDetail.js
@@ -1,214 +1,27 @@
 import './WorkItemDetail.less'
 import { StarFilled, EyeFilled } from '@ant-design/icons'
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import ThumbUp from '../ThumbUp'
 import picPic from '../../../assets/imgs/homePage/picture.jpg'
+import { apiGet } from '@/service/reqMethod'
 
-import { apiGet, apiPost } from '@/service/reqMethod'
-
-const list = [
-  {
-    pic: picPic,
-    title: '随风集',
-    author: '金昌友',
-    type: '网页，企业官网',
-    thumb: '57',
-    eye: '1990',
-    publishTime: '22小时前',
-  },
-  {
-    pic: picPic,
-    title: '草木茶舍 | 泡茶古朴又惊艳',
-    author: '黄冠之',
-    type: '空间，空间餐饮',
-    thumb: '69',
-    eye: '3018',
-    publishTime: '1天前',
-  },
-  {
-    pic: picPic,
-    title: '字体设计 | Font design',
-    author: '戴秀',
-    type: '平面，字体',
-    thumb: '157',
-    eye: '6990',
-    publishTime: '2天前',
-  },
-  {
-    pic: picPic,
-    title: '随风集',
-    author: '金昌友',
-    type: '网页，企业官网',
-    thumb: '57',
-    eye: '1990',
-    publishTime: '22小时前',
-  },
-  {
-    pic: picPic,
-    title: '随风集',
-    author: '金昌友',
-    type: '网页，企业官网',
-    thumb: '57',
-    eye: '1990',
-    publishTime: '22小时前',
-  },
-  {
-    pic: picPic,
-    title: '随风集',
-    author: '金昌友',
-    type: '网页，企业官网',
-    thumb: '57',
-    eye: '1990',
-    publishTime: '22小时前',
-  },
-  {
-    pic: picPic,
-    title: '随风集',
-    author: '金昌友',
-    type: '网页，企业官网',
-    thumb: '57',
-    eye: '1990',
-    publishTime: '22小时前',
-  },
-  {
-    pic: picPic,
-    title: '随风集',
-    author: '金昌友',
-    type: '网页，企业官网',
-    thumb: '57',
-    eye: '1990',
-    publishTime: '22小时前',
-  },
-  {
-    pic: picPic,
-    title: '随风集',
-    author: '金昌友',
-    type: '网页，企业官网',
-    thumb: '57',
-    eye: '1990',
-    publishTime: '22小时前',
-  },
-  {
-    pic: picPic,
-    title: '随风集',
-    author: '金昌友',
-    type: '网页，企业官网',
-    thumb: '57',
-    eye: '1990',
-    publishTime: '22小时前',
-  },
-  {
-    pic: picPic,
-    title: '随风集',
-    author: '金昌友',
-    type: '网页，企业官网',
-    thumb: '57',
-    eye: '1990',
-    publishTime: '22小时前',
-  },
-  {
-    pic: picPic,
-    title: '随风集',
-    author: '金昌友',
-    type: '网页，企业官网',
-    thumb: '57',
-    eye: '1990',
-    publishTime: '22小时前',
-  },
-  {
-    pic: picPic,
-    title: '随风集',
-    author: '金昌友',
-    type: '网页，企业官网',
-    thumb: '57',
-    eye: '1990',
-    publishTime: '22小时前',
-  },
-  {
-    pic: picPic,
-    title: '随风集',
-    author: '金昌友',
-    type: '网页，企业官网',
-    thumb: '57',
-    eye: '1990',
-    publishTime: '22小时前',
-  },
-  {
-    pic: picPic,
-    title: '随风集',
-    author: '金昌友',
-    type: '网页，企业官网',
-    thumb: '57',
-    eye: '1990',
-    publishTime: '22小时前',
-  },
-  {
-    pic: picPic,
-    title: '随风集',
-    author: '金昌友',
-    type: '网页，企业官网',
-    thumb: '57',
-    eye: '1990',
-    publishTime: '22小时前',
-  },
-  {
-    pic: picPic,
-    title: '随风集',
-    author: '金昌友',
-    type: '网页，企业官网',
-    thumb: '57',
-    eye: '1990',
-    publishTime: '22小时前',
-  },
-  {
-    pic: picPic,
-    title: '随风集',
-    author: '金昌友',
-    type: '网页，企业官网',
-    thumb: '57',
-    eye: '1990',
-    publishTime: '22小时前',
-  },
-  {
-    pic: picPic,
-    title: '随风集',
-    author: '金昌友',
-    type: '网页，企业官网',
-    thumb: '57',
-    eye: '1990',
-    publishTime: '22小时前',
-  },
-  {
-    pic: picPic,
-    title: '随风集',
-    author: '金昌友',
-    type: '网页，企业官网',
-    thumb: '57',
-    eye: '1990',
-    publishTime: '22小时前',
-  },
-]
 const WorkItemDetail = () => {
-  const getApiData = () => {
+  const [list, setList] = useState([])
+  useEffect(() => {
     apiGet('/workItemDetailList', { a: 33 }).then((res) => {
-      console.log(res)
+      setList(res.data)
     })
-    apiPost('/testPost', { a: 1 }).then((res) => {
-      console.log(res)
-    })
-  }
+  }, [])
+
   return (
     <div className="works-part-content-container">
-      <br />
-      <button onClick={getApiData}>点击获取数据</button>
-      <br />
-      <br />
       {list.map((item, index) => (
         <div className="works-part-item" key={index}>
           <div className="works-part-content">
             <div className="wp-img">
               <Link>
-                <img src={item.pic} alt={item.title} />
+                <img src={picPic} alt={item.title} />
               </Link>
             </div>
             <div className="wp-title">
